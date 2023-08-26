f8aa7b4d16c02d9dab63636d8fdb9814da822ab0
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2023-08-26 17:58:31 +0000

    Add tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -20,6 +20,10 @@ abstract class TestCase extends BaseTestCase
         Waterline::auth(function () {
             return true;
         });
+
+        if (! class_exists('\Workflow\Models\Model')) {
+            class_alias(\Illuminate\Database\Eloquent\Model::class, '\Workflow\Models\Model');
+        }
     }
 
     protected function getEnvironmentSetUp($app)
